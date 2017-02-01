d3193ae3cbd3b1bd6688de397f3761c6573edff1
Milan Gupta <mgup64@sapient.com>
2017-02-01 18:49:20 +0530

    resolved bug in delete todo

--- src/reducers/reducer_add_todo.js
+++ src/reducers/reducer_add_todo.js
@@ -1,13 +1,13 @@
 import {ADD_TODO, DELETE_TODO,EDIT_TODO,EDIT_ITEM_MODE,TOGGLE_COMPLETE} from '../actions/index';
 export default function(state = [], action){
 	var stateArr = state.slice(0)
-	console.log(state)
 	switch(action.type){
 		case ADD_TODO :
 		//We can also use state.concat([action.payload.data]) as it dont mutate state
 			return [ ...state, {id: state.length, term:action.payload, isEditMode:false,isCompleted:false}]
 		case DELETE_TODO :
-			return [...state.slice(0,action.payload), ...state.slice(action.payload+1)]
+			return stateArr.filter((item)=> item.id!==action.payload)
+			
 		case EDIT_TODO :
 			for(let item of stateArr){
 				if(item.id === action.payload.id){
